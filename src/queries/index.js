d693d31048bd65e5c8794a2ffa76f8da09ade419
juhyeon1114 <juhyeon1114@gmail.com>
2022-10-04 15:22:46 +0900

    Optimized RQ

--- src/queries/index.js
+++ src/queries/index.js
@@ -35,13 +35,33 @@ export const useGetSportsQuery = (p) => {
 export const useCreateSport = (data) => {
   const client = useQueryClient()
   return useMutation(createSport, {
-    onSuccess: () => {
+    onSuccess: (data) => {
       /**
        * .invalidateQueries()
        * -> get-sports 쿼리를 stale한 상태로 만듬.
        * -> 즉, 자동적으로 해당 데이터가 보여지는 컴포넌트가 마운트 될 때, query가 다시 실행됨
        */
-      client.invalidateQueries(['get-sports'])
+      // client.invalidateQueries(['get-sports'])
+
+      /**
+       * Mutation실행 후, get-sports의 query데이터를 새로 실행해줌
+       */
+      client.setQueryData('get-sports', (oldData) => {
+        return {
+          ...oldData,
+          data: [...oldData.data, data.data],
+        }
+      })
+    },
+    onMutate: (newData) => {
+      console.log('Mutation이 실행될 때, 실행됨', newData)
+      // client.cancelQueries('get-sports')
+    },
+    onError: (_error, _data, context) => {
+      console.log('Query가 에러났을 때')
+    },
+    onSettled: () => {
+      console.log('Query의 성공 여부에 상관없이 query의 실행이 끝난 후 항상 실행됨')
     },
   })
 }
